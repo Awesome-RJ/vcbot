4db5aae5fc9c8c3137942f6c373bec9b01b8e298
Tc Bots <85434104+TcBots@users.noreply.github.com>
2021-06-08 23:14:56 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -21,8 +21,8 @@ import sys
 import time
 from helpers.errors import DurationLimitError
 
-@Client.on_message(filters.command('song') & ~filters.channel)
-def song(client, message):
+@Client.on_message(filters.command('sng') & ~filters.channel)
+def sng(client, message):
 
     user_id = message.from_user.id 
     user_name = message.from_user.first_name 
@@ -61,7 +61,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**🎵 Uploaded by @TcPlayerBot **'
+        rep = '**🎵 Uploaded by @Tc_Bots **'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
